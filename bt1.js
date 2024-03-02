d0c518ada17fa33bcc850de5b11c05d8423cdb40
DESKTOP-R4TO9PD\ADMIN <linhnhi.2922@gmail.com>
2024-03-02 20:08:13 +0700

    init project

---
+++ bt1.js
@@ -0,0 +1,18 @@
+var books = [
+    {
+        tacGia : 'NguuyenThiLinhNhi',
+        tieuDe : 'Books1',
+        namXuatBan :'2024',
+        trangThai : true
+    },
+    {
+        tacGia : 'NguuyenThiLinhNhi',
+        tieuDe : 'Books1',
+        namXuatBan :'2024',
+        trangThai : true
+    }
+]
+books.forEach((item)=>{
+   console.log(`tacGia : ${item.tacGia} tieuDe : ${item.tieuDe}  namXuatBan : ${item.namXuatBan} TrangThai : ${item.trangThai}`)
+})
+
